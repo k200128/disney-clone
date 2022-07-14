7b7f4ac8eaed8020b0138510bffcd671841a236c
k200128 <aparth76@gmail.com>
2022-07-14 15:45:30 +1000

    first commit

---
+++ src/components/Detail.js
@@ -0,0 +1,146 @@
+import React from 'react'
+import styled from 'styled-components'
+function Detail() {
+  return (
+    <Container>
+        <Background>
+            <img src="https://4.bp.blogspot.com/-KuWXfA_2zEw/WsPYrBXXXOI/AAAAAAAAVyw/1cMGP21yZcU5Y9uEKNGHuu5LabzGdKGfwCLcBGAs/s1600/Bao-Lead-Image.jpg" alt="" />
+        </Background>
+        <ImageTitle>
+            <img src="/images/pixar-bao-logo.png" alt="" />
+        </ImageTitle>
+        <Controls>
+            <PlayButton>
+                <img src="/images/play-icon-black.png"  />
+                <span> PLAY</span>
+                </PlayButton>
+            <TrailerButton>
+            <img src="/images/play-icon-white.png" alt="" />
+            <span>
+                TRAILER
+            </span>
+
+            </TrailerButton>
+            <AddButton>
+                <span>
+                    +
+                </span>
+            </AddButton>
+    
+
+            <GroupWatchButton>
+                <img src="/images/group-icon.png" />
+
+            </GroupWatchButton>
+
+        </Controls>
+        <SubTitle>
+           2022 . 7m . Family, Fantasy, Kids, Anmation 
+        </SubTitle>
+        <Description>
+        The film is about an aging and lonely Chinese Canadian mother suffering from empty nest syndrome, who receives an unexpected second chance at motherhood when she makes a steamed bun (baozi) that comes to life.
+        </Description>
+        </Container>
+  )
+}
+
+export default Detail
+
+const Container = styled.div`
+min-height: calc(100vh - 70px);
+padding: 0 calc(3.5vw + 5px);
+position: relative;
+
+`
+const Background = styled.div`
+
+position: fixed;
+top:0,
+right:0;
+left:0;
+bottom:0;
+z-index: -1;
+opacity:0.9;
+img {
+    width: 100vw;
+    height: 100vh;
+    object-fit:cover;
+}
+
+`
+const ImageTitle = styled.div`
+height:30vh;
+min-height:170px;
+min-width:200px;
+width:35vw;
+margin-top:60px;
+img {
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+
+}
+`
+const Controls=styled.div`
+display:flex;
+align-items:center;
+`
+const PlayButton = styled.button`
+border-radius:4px;
+font-size:15px;
+padding:0 24px;
+margin-right:22px;
+display:flex;
+align-items:center;
+height:56px;
+backgound:rgb(249,249,249);
+border:none;
+letter-spacing:1.8px;
+cursor:pointer;
+
+&:hover{
+    background rgb(198, 198, 198)
+}
+
+
+`
+const TrailerButton= styled(PlayButton)`
+background:rgba( 0, 0, 0, 0.3 );
+border: 1px solid rgb(249, 249, 249);
+color:rgb(249, 249, 249);
+
+`
+const AddButton = styled.button`
+width:44px;
+height:44px;
+display:flex;
+align-items :center;
+justify-content:center;
+border-radius:50%;
+border: 2px solid white;
+background-color:rgba(0, 0, 0, 0.6);
+cursor:pointer;
+margin-right:16px;
+
+span{
+    font-size:30px;
+    color:white;
+}
+`
+const GroupWatchButton= styled(AddButton)`
+background:rgb(0, 0, 0);
+`
+const SubTitle = styled.div`
+color:(249, 249, 249);
+font-size:15px;
+min-height:20px;
+margin-top:26px;
+
+`
+const Description= styled.div`
+line-height:1.4;
+font-size:20px;
+margin-top:16px;
+color: (249, 249, 249);
+max-width:70%;
+`
--- src/components/Header.js
+++ src/components/Header.js
@@ -44,6 +44,8 @@ background: #090b13;
 display:flex;
 align-items :center;
 padding:0 36px;
+overflow-x:hidden;
+
 
 `
 const Logo =styled.img`
